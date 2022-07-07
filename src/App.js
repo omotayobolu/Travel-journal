532c0138edf1dc0befd4be64e335035fb1e9d940
Boluwatife <omotayodaniel10@gmail.com>
2022-07-07 13:19:00 +0100

    main styles left

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Data from "./components/Data";
 
 export default function App() {
+  const details = Data.map((item) => {
+    return <Main item={item} />;
+  });
   return (
     <div>
       <Navbar />
+      <section className="journal-details">{details}</section>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,64 +1,26 @@
 import React from "react";
-import WebImg from "./Webimg";
-import {
-  FaTwitter,
-  FaFacebookF,
-  FaInstagram,
-  FaGithub,
-  FaLinkedin,
-} from "react-icons/fa";
-import { HiMail } from "react-icons/hi";
+import location from "../images/Fill 220.png";
 
-export default function Main() {
+export default function Main(props) {
   return (
-    <div>
-      <div className="container">
-        <WebImg />
-        <main className="content">
-          <div className="intro-texts">
-            <h2 className="name">Laura Smith</h2>
-            <p className="job">Frontend Developer</p>
-            <p className="website">laurasmith.website</p>
-          </div>
-          <div className="contact-cta">
-            <button className="btn email">
-              <HiMail className="btn-icon" />
-              Email
-            </button>
-            <button className="btn linkedIn">
-              <FaLinkedin className="btn-icon" />
-              LinkedIn
-            </button>
-          </div>
-          <div className="more-info">
-            <div className="about">
-              <h3>About</h3>
-              <p className="more-info-text">
-                I am a frontend developer with a particular interest in making
-                things simple and automating dily tasks. I try to keep up with
-                security and best practices, and I'm always looking for new
-                things to learn.
-              </p>
-            </div>
-            <div className="interests">
-              <h3>Interests</h3>
-              <p className="more-info-text">
-                Food expert. Music scholar. Reader. Internet fanatic. Bacon
-                buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee
-                fanatic.
-              </p>
-            </div>
-          </div>
-        </main>
-        <footer>
-          <div className="socials">
-            <FaTwitter className="social" />
-            <FaFacebookF className="social" />
-            <FaInstagram className="social" />
-            <FaGithub className="social" />
-          </div>
-        </footer>
+    <section className="main">
+      <div className="main-image">
+        <img src={props.item.image} class="location-image" alt="" />
       </div>
-    </div>
+      <div className="main-content-1">
+        <img src={location} alt="" />
+        <span className="country">{props.item.location}</span>
+        <a href="wwww.googlemap.com" className="map">
+          View on Google Maps
+        </a>
+      </div>
+      <div className="main-content-2">
+        <h1>{props.item.title}</h1>
+        <h4 className="date">
+          ({props.item.date.start} - {props.item.date.end})
+        </h4>
+        <p className="description">{props.item.description}</p>
+      </div>
+    </section>
   );
 }
